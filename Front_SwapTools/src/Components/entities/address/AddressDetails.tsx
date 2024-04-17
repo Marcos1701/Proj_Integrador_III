2f65b35278eb5bf32316529f3cc13e014fa401ef
Marcos1701 <catce.2022111tads0428@aluno.ifpi.edu.br>
2024-04-16 21:03:39 -0300

    :busts_in_silhouette: Definindo e Aprimorando componentes referentes ao formulário e aos detalhes de endereço

--- Front_SwapTools/src/Components/entities/address/AddressDetails.tsx
+++ Front_SwapTools/src/Components/entities/address/AddressDetails.tsx
@@ -1,3 +1,6 @@
+import axios, { AxiosResponse } from "axios";
+import { object, string, number, date, InferType } from "yup";
+
 export interface GeoPoint {
 	// Latitude da localização.
 	latitude: number;
@@ -39,3 +42,67 @@ export interface DefaultAddress {
 	// ID do endereço padrão.
 	addressId: string;
 }
+
+export const AddressSchema = object().shape({
+	id: string().required(),
+	userId: string().required(),
+	street: string().required(),
+	number: string().required(),
+	complement: string(),
+	city: string().required(),
+	state: string().required(),
+	country: string().required(),
+	zipCode: string().required(),
+	createdAt: string().required(),
+	updatedAt: string().required(),
+	geoPoint: object()
+		.shape({
+			latitude: number().required(),
+			longitude: number().required(),
+		})
+		.required(),
+}); // serve para validar os dados de um endereço
+
+export type AddressType = InferType<typeof AddressSchema>; // serve para inferir o tipo de um endereço
+
+export const DefaultAddressSchema = object().shape({
+	userId: string().required(),
+	addressId: string().required(),
+}); // serve para validar os dados de um endereço padrão
+
+export type DefaultAddressType = InferType<typeof DefaultAddressSchema>; // serve para inferir o tipo de um endereço padrão
+
+async function fetchAddress(addressId: string): Promise<AddressType> {
+	try {
+		// Busca o endereço no banco de dados.
+
+		const response: AxiosResponse<AddressType> = await axios.get<AddressType>(
+			`/addresses/${addressId}`,
+		);
+
+		if (response.status !== 200) {
+			throw new Error("Failed to fetch address");
+		}
+		return response.data;
+	} catch (error) {
+		if (error instanceof Error) {
+			throw new Error(
+				`Failed to fetch address details: ${error.message}` || "Unknown error",
+			);
+		}
+		throw error;
+	}
+}
+
+async function getAddressDetails(addressId: string): Promise<AddressType> {
+	// Busca o endereço no banco de dados.
+	const address: AddressType = await fetchAddress(addressId);
+	// Valida os dados do endereço.
+	await AddressSchema.validate(address);
+	// Retorna os dados do endereço.
+	return address;
+}
+
+export const AddressDetails = (): React.ReactElement => {
+	return <div>AddressDetails</div>;
+};
