56facc2bde8d7e319505188fccbb2348a911a34b
Marcos1701 <catce.2022111tads0428@alunoo.ifpi.edu.br>
2024-05-15 21:26:38 -0300

    :tada: adicionando dinamica básica de login

--- Back_end/libs/database/src/database.module.ts
+++ Back_end/libs/database/src/database.module.ts
@@ -2,23 +2,33 @@ import { Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { User, Photo, RefreshToken,Address } from './entities';
+import { User, Photo, RefreshToken, Address } from './entities';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_DB,
+} = process.env;
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'test',
+      host: POSTGRES_HOST,
+      port: parseInt(POSTGRES_PORT),
+      username: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      database: POSTGRES_DB,
       entities: [RefreshToken, User, Photo, Address],
       synchronize: true,
-    })
+    }),
   ],
   providers: [DatabaseService],
   exports: [DatabaseService],
 })
-export class DatabaseModule { }
+export class DatabaseModule {}
